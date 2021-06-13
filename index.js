ba80d1a5cd1ebcd92cedaeeed7983241ac57c126
Rabin Gaire <rabingaire20@gmail.com>
2021-06-13 11:15:41 +0545

    update code from callback to async await

--- index.js
+++ index.js
@@ -8,60 +8,53 @@ const icons = require("./icons.json");
 
 const Oauth1Helper = require("./oauth");
 
-function main() {
-  const openWeatherUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=:lat&lon=:lon&exclude=minutely,hourly,daily,alerts&appid=:apiKey";
+async function main() {
+  try {
+    const openWeatherUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=:lat&lon=:lon&exclude=minutely,hourly,daily,alerts&appid=:apiKey";
 
-  const twitterUrl =
-    "https://api.twitter.com/1.1/account/update_profile.json?name=:name";
+    const twitterUrl =
+      "https://api.twitter.com/1.1/account/update_profile.json?name=:name";
 
-  const url = openWeatherUrl
-    .replace(":lat", process.env.LATITUDE)
-    .replace(":lon", process.env.LOGITUDE)
-    .replace(":apiKey", process.env.OPEN_WEATHER_API_KEY);
+    const url = openWeatherUrl
+      .replace(":lat", process.env.LATITUDE)
+      .replace(":lon", process.env.LOGITUDE)
+      .replace(":apiKey", process.env.OPEN_WEATHER_API_KEY);
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((value) => {
-      const { icon: type } = value.current.weather[0];
+    const response = await fetch(url);
+    const value = await response.json();
+    const { icon: type } = value.current.weather[0];
 
-      // send request to twitter
-      const url = twitterUrl.replace(":name", encodeURI(icons[type]));
+    // send request to twitter
+    const request = {
+      url: twitterUrl.replace(":name", encodeURI(icons[type])),
+      method: "POST",
+    };
 
-      const request = {
-        url: url,
-        method: "POST",
-      };
+    const authHeader = Oauth1Helper.getAuthHeaderForRequest(request);
 
-      const authHeader = Oauth1Helper.getAuthHeaderForRequest(request);
-
-      fetch(request.url, {
-        method: request.method,
-        headers: authHeader,
-      })
-        .then((response) => response.json())
-        .then(() => {
-          console.log(
-            `User name is updated. Icon type ${type}: ${icons[type]}`
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    })
-    .catch((err) => {
-      console.error(err);
+    await fetch(request.url, {
+      method: request.method,
+      headers: authHeader,
     });
+
+    console.log(`User name is updated. Icon type ${type}: ${icons[type]}`);
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
-// deta cron specific code
-if (process.env.DETA_RUNTIME) {
-  app.lib.cron(() => {
-    main();
-    return "Cron ran";
-  });
-} else {
-  main();
-}
+(async function () {
+  // deta cron specific code
+  if (process.env.DETA_RUNTIME) {
+    app.lib.cron(async () => {
+      await main();
+      return "Cron ran successfully";
+    });
+  } else {
+    await main();
+    console.log("Program ran successfully");
+  }
+})();
 
 module.exports = app;
